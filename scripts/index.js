116b5e5d242b42023f8f1c36d40f5685c3db205c
WilliamGichora <markgichora7@gmail.com>
2024-04-01 14:03:29 +0300

    i have concluded most of the frontend. Added a Dashboard to manage users, posts and categories

--- scripts/index.js
+++ scripts/index.js
@@ -9,9 +9,28 @@ const openNav = () => {
 };
 openNavBtn.addEventListener('click', openNav);
 
-const closeNav=()=>{
-    navItems.style.display= 'none';
-    closeNavBtn.style.display='none';
+const closeNav = () => {
+    navItems.style.display = 'none';
+    closeNavBtn.style.display = 'none';
     openNavBtn.style.display = 'inline-block'
 }
 closeNavBtn.addEventListener('click', closeNav);
+
+//Code to toggle the side bar in mobile screen sizes
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show_sidebar');
+const hideSidebarBtn = document.querySelector('#hide_sidebar');
+
+const showSideBar = () => {
+    sidebar.style.left= '0';
+    showSidebarBtn.style.display='none';
+    hideSidebarBtn.style.display= 'inline-block';
+}
+showSidebarBtn.addEventListener('click', showSideBar);
+
+const hideSideBar = () => {
+    sidebar.style.left= '-100%';
+    showSidebarBtn.style.display='inline-block';
+    hideSidebarBtn.style.display= 'none';
+}
+hideSidebarBtn.addEventListener('click', hideSideBar);
