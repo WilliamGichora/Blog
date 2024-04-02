615fc59194bb2d47b359b1a0e8c7930b34735d5a
WilliamGichora <markgichora7@gmail.com>
2024-04-02 17:39:59 +0300

    Set up a database connection

--- Admin Pages/Admin Configurations/database.php
+++ Admin Pages/Admin Configurations/database.php
@@ -1,1 +1,7 @@
 <?php require 'constants.php';
+
+//connecting the database
+$conn= new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if (mysqli_errno($conn)) {
+    die(mysqli_error($conn));
+}
--- Configurations/constants.php
+++ Configurations/constants.php
@@ -1,3 +1,7 @@
 <?php 
 define('ROOT_URL','http://localhost/Blog/');
 //define('ROOT2', 'http://' . $_SERVER['HTTP_HOST'] . '/index.php');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','blog_db');
